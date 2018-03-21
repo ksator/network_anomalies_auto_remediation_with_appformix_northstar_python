386bfc7fdf7dbf0e6fa2b3cec153c808d6739d84
ksator <khelil.sator.git@gmail.com>
2018-03-22 00:25:22 +0100

    m

---
+++ render_devices.py
@@ -0,0 +1,14 @@
+from jinja2 import Template
+from yaml import load
+
+f=open('devices.yml', 'r')
+my_vars=load(f.read())
+f.close()
+
+f=open('templates/devices_jti.j2')
+my_template=Template(f.read())
+f.close()
+
+f=open('network_devices.json','w')
+f.write(my_template.render(my_vars))
+f.close()
