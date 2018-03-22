d1f39dd4ced61e53b53e8a0573146a633609467a
Khelil Sator <khelil.sator.git@gmail.com>
2018-03-22 21:11:56 +0100

    Create configure_snmp.py

---
+++ configure_snmp.py
@@ -0,0 +1,24 @@
+from jinja2 import Template
+from yaml import load
+from jnpr.junos import Device
+from jnpr.junos.utils.config import Config
+
+f=open('devices.yml', 'r')
+my_vars = load(f.read())
+f.close()
+
+f=open('snmp.j2')
+my_template = Template(f.read())
+f.close()
+
+f=open('snmp.conf','w')
+f.write(my_template.render(my_vars))
+f.close()
+
+for item in snmp_2c.devices: 
+    device=Device (host="item.ip", user="jti.username", password="jti.password")
+    device.open()
+    cfg=Config(device) 
+    cfg.load(path='snmp.conf', format='set')
+    cfg.commit()
+    device.close()
