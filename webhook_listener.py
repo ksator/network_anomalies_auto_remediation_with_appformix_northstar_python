90e1825eb803f29e20ac044bc15565f9c8b96944
Khelil Sator <khelil.sator.git@gmail.com>
2018-03-26 00:18:21 +0200

    Create  webhook_listener.py

---
+++ webhook_listener.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, abort, make_response, request
+import northstar
+
+app = Flask(__name__)
+
+@app.route('/northstar/device/maintenance', methods=['POST'])
+def node_in_maintenance():
+ if request.headers['Content-Type'] != 'application/json':
+        abort(400, message="Expected Content-Type = application/json")
+ data = request.json
+ dev = data["device"]
+ northstar.put_device_in_maintenance(dev)
+ return jsonify({'device in maintenance ': dev}), 201
+
+app.run(
+    debug=True,
+    port=int("12345"),
+    host="0.0.0.0"
+    )
+
