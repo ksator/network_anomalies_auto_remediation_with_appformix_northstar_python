c41f2d2c7272e65427cb8a65f4b021b4e2545743
ksator <khelil.sator.git@gmail.com>
2018-03-26 15:07:01 +0200

    m

--- junos_devices.py
+++ junos_devices.py
@@ -1,7 +1,10 @@
 from jnpr.junos import Device
 from jnpr.junos.utils.config import Config
+import os
 
 def collect_junos_commands(dev):
+    if not os.path.exists(dev):
+        os.makedirs(dev)
     device=Device (host=dev, user='lab', password='m0naco')
     device.open()
     commands = ["show version", "show chassis hardware", "show interfaces extensive"]
