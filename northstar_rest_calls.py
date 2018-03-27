7d6d57b3f0995b82929d1bb12c060d099183bb26
Khelil Sator <khelil.sator.git@gmail.com>
2018-03-27 21:23:01 +0200

    Update northstar_rest_calls.py

--- northstar_rest_calls.py
+++ northstar_rest_calls.py
@@ -7,16 +7,13 @@ import yaml
 import json
 
 def put_device_in_maintenance(dev):
-
  requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
  authuser = 'admin'
  authpwd = 'Embe1mpls'
-
  headers = { 'content-type' : 'application/json'}
  url = 'https://10.102.180.160:8443/oauth2/token'
  data_to_get_token = {"grant_type":"password","username":authuser,"password":authpwd}
  r = requests.post(url, data=json.dumps(data_to_get_token), auth=(authuser, authpwd), headers=headers, verify=False)
-
  headers = {'Authorization':str('Bearer ' + r.json()['access_token']), 'Accept' : 'application/json', 'Content-Type' : 'application/json'}
  url_base = 'http://10.102.180.160:8091/NorthStar/API/v2/tenant/'
  url = url_base + '1/topology/1/nodes'
@@ -24,7 +21,6 @@ def put_device_in_maintenance(dev):
  for i in r.json():
    if i['hostName'] == dev:
     node_index = i['nodeIndex']
-
  maintenance_url = url_base + '1/topology/1/maintenances'
  maintenance_data = {
      "topoObjectType": "maintenance",
