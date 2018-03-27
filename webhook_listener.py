73123e158ec12bae88642aba081d21f654d7a774
Khelil Sator <khelil.sator.git@gmail.com>
2018-03-27 21:12:26 +0200

    Update webhook_listener.py

--- webhook_listener.py
+++ webhook_listener.py
@@ -1,15 +1,17 @@
 from flask import Flask, jsonify, abort, make_response, request
 import northstar_rest_calls
 import push_to_git
+import json
+from pprint import pprint
 
 app = Flask(__name__)
 
-@app.route('/northstar/device_maintenance', methods=['POST'])
+@app.route('/northstar/put_device_in_maintenance', methods=['POST'])
 def node_in_maintenance():
  if request.headers['Content-Type'] != 'application/json':
         abort(400, message="Expected Content-Type = application/json")
  data = request.json
- dev = data["device"]
+ dev = data['status']['entityId']
  northstar_rest_calls.put_device_in_maintenance(dev)
  return jsonify({'device in maintenance ': dev}), 201
 
@@ -18,10 +20,17 @@ def collect_commands():
  if request.headers['Content-Type'] != 'application/json':
         abort(400, message="Expected Content-Type = application/json")
  data = request.json
- dev = data["device"]
+ dev = data['status']['entityId']
  push_to_git.collect_and_push(dev)
  return jsonify({'collected data on device ': dev}), 201
 
+
+@app.route('/test', methods=['POST'])
+def print_device_name():
+ data = request.json
+ device_id = data['status']['entityId']
+ print device_id
+ return device_id
 
 app.run(
     debug=True,
