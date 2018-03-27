f9fd78c8f5a97c72c2bea77158050edb22afccc1
ksator <ksatator@juniper.net>
2018-03-27 04:30:22 -0700

    from Gitpython

--- webhook_listener.py
+++ webhook_listener.py
@@ -9,8 +9,8 @@ def node_in_maintenance():
  if request.headers['Content-Type'] != 'application/json':
         abort(400, message="Expected Content-Type = application/json")
  data = request.json
- #dev = data["device"]
- dev='10.49.124.59'
+ dev = data["device"]
+ #dev='10.49.124.59'
  northstar_rest_calls.put_device_in_maintenance(dev)
  return jsonify({'device in maintenance ': dev}), 201
 
@@ -19,8 +19,8 @@ def collect_commands():
  if request.headers['Content-Type'] != 'application/json':
         abort(400, message="Expected Content-Type = application/json")
  data = request.json
- #dev = data["device"]
- dev='10.49.124.59'
+ dev = data["device"]
+ #dev='10.49.124.59'
  push_to_git.collect_and_push(dev)
  return jsonify({'collected data on device ': dev}), 201
 
