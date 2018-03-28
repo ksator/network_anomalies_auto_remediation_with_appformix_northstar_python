aabbb3ffd0e32b573caaf1075e0f4c4fed4af5d1
ksator <ksatator@juniper.net>
2018-03-28 05:03:45 -0700

    m

--- northstar_rest_calls.py
+++ northstar_rest_calls.py
@@ -11,11 +11,11 @@ def put_device_in_maintenance(dev):
  authuser = 'admin'
  authpwd = 'Embe1mpls'
  headers = { 'content-type' : 'application/json'}
- url = 'https://10.102.180.160:8443/oauth2/token'
+ url = 'https://10.49.124.229:8443/oauth2/token'
  data_to_get_token = {"grant_type":"password","username":authuser,"password":authpwd}
  r = requests.post(url, data=json.dumps(data_to_get_token), auth=(authuser, authpwd), headers=headers, verify=False)
  headers = {'Authorization':str('Bearer ' + r.json()['access_token']), 'Accept' : 'application/json', 'Content-Type' : 'application/json'}
- url_base = 'http://10.102.180.160:8091/NorthStar/API/v2/tenant/'
+ url_base = 'http://10.49.124.229:8091/NorthStar/API/v2/tenant/'
  url = url_base + '1/topology/1/nodes'
  r = requests.get(url, headers=headers, verify=False)
  for i in r.json():
