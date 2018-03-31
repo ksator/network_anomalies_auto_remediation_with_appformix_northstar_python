0ba1b80462ed79ddcf72e25568aec97c047b2127
ksator <ksator@juniper.net>
2018-04-01 00:18:28 +0200

    m

--- network_devices.py
+++ network_devices.py
@@ -1,29 +1,14 @@
-from jnpr.junos import Device
-from jnpr.junos.utils.config import Config
-import os
+from jinja2 import Template
 from yaml import load
 
-def collect_junos_commands(dev):
-    if not os.path.exists(dev):
-        os.makedirs(dev)
-    f=open('network_devices.yml', 'r')
-    my_vars = load(f.read())
-    f.close()
-    if my_vars['use_snmp_2c']:
-       for item in my_vars['snmp_2c']['devices']:
-          if item['name'] == dev:
-             dev_ip =  item['ip']
-    if my_vars['use_jti']:
-       for item in my_vars['jti']['devices']:
-          if item['name'] == dev:
-             dev_ip =  item['out_of_band']
-    device=Device (host=dev_ip, user='northstar', password='Embe1mpls')
-    device.open()
-    commands = ["show version", "show chassis hardware", "show interfaces extensive"]
-    for item in commands:
-        cli = device.cli(item, warning=False)
-        f = open(dev +'/'+ item + '.txt', 'w')
-        f.write(cli)
-        f.close()
-    device.close()
-    return 'done'
+f=open('network_devices.yml', 'r')
+my_vars = load(f.read())
+f.close()
+
+f=open('network_devices.j2')
+my_template = Template(f.read())
+f.close()
+
+f=open('network_devices.json','w')
+f.write(my_template.render(my_vars))
+f.close()
